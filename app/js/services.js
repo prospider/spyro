f7262048dfc15a0ee2a8d8b91047d5931a681ab9
David Bel <dbel.tbay@gmail.com>
2014-08-15 22:43:10 -0400

    fixed missing user dependency in messageList
    Also updated bower.json

--- app/js/services.js
+++ app/js/services.js
@@ -3,17 +3,17 @@
 
    /* Services */
 
-   angular.module('spyro.services', [])
+   angular.module('spyro.services', ['firebase.utils', 'simpleLogin'])
 
       // put your services here!
       // .service('serviceName', ['dependency', function(dependency) {}]);
 
-     .service('messageList', ['fbutil', 'user', function(fbutil, user) {
+     .service('messageList', ['fbutil', 'simpleLogin', function(fbutil, simpleLogin) {
        this.messages = fbutil.syncArray('messages', {limit: 10, endAt: null});
 
         this.addMessage = function(newMessage) {
           var callback = newMessage.callback || "none";
-          var loggedInUser = user.uid;
+          var loggedInUser = simpleLogin.getUser().uid;
 
           this.messages.$add({
             type: newMessage.type,
